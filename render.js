127198aec4445aebc42eb48b84f4df3e8ce4368f
Kostja27 <78737821+Kostja27@users.noreply.github.com>
2022-09-16 02:19:19 +0300

    Add App

---
+++ render.js
@@ -0,0 +1,202 @@
+function render(store, condition) {
+    if (condition) {
+        store.map(function (items, id) {
+            if (items.Statuse == true) {
+                const elem = document.querySelector(".TabeleDateInfo")
+                const newElement = document.createElement("div");
+                newElement.id = `items_${id}`
+                newElement.classList.add("row")
+                newElement.innerHTML = `<span class="rowDate">${items.Name}</span> <span class="rowDate">${items.Craeted}</span><span class="rowDate">${items.Categori}</span><span class="rowDate">
+       ${items.Content}</span><span class="rowDate">${items.Dates}</span><span class="rowDate"><button class='change'>change</button><button class='onArchive'>arch</button><button class='remove'>Del</button></span>`;
+                elem.after(newElement)
+            }
+        })
+    } else if (condition == false) {
+        store.map(function (items, id) {
+            if (items.Statuse == true) {
+                const elem = document.querySelector(".TabeleDateInfo")
+                const newElement = document.createElement("div");
+                newElement.id = `items_${id}`
+                newElement.classList.add("row")
+                newElement.innerHTML = `<span class="rowDate">${items.Name}</span> <span class="rowDate">${items.Craeted}</span><span class="rowDate">${items.Categori}</span><span class="rowDate">
+       ${items.Content}</span><span class="rowDate">${items.Dates}</span><span class="rowDate"><button class='change'>change</button><button class='onArchive'>arch</button><button class='remove'>Del</button></span>`;
+                elem.after(newElement)
+            }
+        })
+    }
+    const elem = document.querySelector(".addTabele")
+    elem.classList.remove("disappear")
+    renderStatistics(store)
+}
+export { render }
+
+
+
+
+function renderArchive(store, condition) {
+    if (condition) {
+        store.map(function (items, id) {
+            if (items.Statuse == true) {
+                const elem = document.querySelector(".TabeleDateInfo")
+                const newElement = document.createElement("div");
+                newElement.id = `items_${id}`
+                newElement.classList.add("row")
+                newElement.innerHTML = `<span class="rowDate">${items.Name}</span> <span class="rowDate">${items.Craeted}</span><span class="rowDate">${items.Categori}</span><span class="rowDate">
+           ${items.Content}</span><span class="rowDate">${items.Dates}</span><span class="rowDate"><button class='onArchive'>arch</button><button class='remove'>Del</button></span>`;
+                elem.after(newElement)
+            }
+        })
+    } else if (condition == false) {
+        store.map(function (items, id) {
+            if (items.Statuse == false) {
+                const elem = document.querySelector(".TabeleDateInfo")
+                const newElement = document.createElement("div");
+                newElement.id = `items_${id}`
+                newElement.classList.add("row")
+                newElement.innerHTML = `<span class="rowDate">${items.Name}</span> <span class="rowDate">${items.Craeted}</span><span class="rowDate">${items.Categori}</span><span class="rowDate">
+           ${items.Content}</span><span class="rowDate">${items.Dates}</span><span class="rowDate"><button class='onArchive'>arch</button><button class='remove'>Del</button></span>`;
+                elem.after(newElement)
+            }
+        })
+    }
+    const elem = document.querySelector(".addTabele")
+    elem.classList.add("disappear")
+    renderStatistics(store)
+}
+export { renderArchive }
+
+
+function renderStatistics(store) {
+    let numberCategori = [
+     {name:"Task",Active:0,Archived:0},
+    { name:"Random Thought",Active:0,Archived:0},
+    {name:'Target',Active:0,Archived:0},
+    {name:'Idea',Active:0,Archived:0}
+    ];
+    numberCategori.map(function(item){ 
+        item.Active+=store.reduce(function(data,items){
+           if(item.name==items.Categori&&items.Statuse==true){return data+1}else{return data}
+        },  0)
+        item.Archived+=store.reduce(function(data,items){
+            if(item.name==items.Categori&&items.Statuse!=true){return data+1}else{return data}
+         },  0)
+
+    })
+
+    numberCategori.forEach(function(items){
+        const elem = document.querySelector(".TabeleStatistics")
+        const newElement = document.createElement("div");
+        newElement.classList.add("rowStatistics")
+        newElement.innerHTML = `<span class="rowDate">${items.name}</span> 
+        <span class="rowDate">${items.Active}</span>
+        <span class="rowDate">${items.Archived}</span>`;
+             elem.after(newElement)
+    })
+}
+
+
+function addInput(){
+                const elem = document.querySelector(".addTabele")
+                const newElement = document.createElement("div");
+                newElement.id = `input`
+                newElement.classList.add("rowInput")
+                newElement.innerHTML=`<input style="width: 15%" class="input1"></input><input class="input2" type="date"></input><select class="input3">
+                <option>Task </option>
+                <option>Random Thought</option>
+                <option>Target</option>
+                <option>Idea</option>
+              </select><textarea  class="input4"></textarea><textarea class="input5"></textarea><div></div>`
+                elem.before(newElement)
+}
+export { addInput }
+
+
+
+function takeDataInput(){
+    let a={ Name:null, Craeted: null, Categori: null, Content: null, Dates:"", Statuse: null }
+    let data1 = document.querySelector(".input1")
+    a.Name=data1.value
+    let data2 = document.querySelector(".input2")
+    a.Craeted=data2.value
+    let data3 = document.querySelector(".input3")
+    a.Categori=data3.value
+    let data4 = document.querySelector(".input4")
+    a.Content=data4.value
+    let data5 = document.querySelector(".input5")
+    a.Dates=data5.value
+    a. Statuse=true
+    return  a
+}
+export { takeDataInput}
+
+
+
+function changeInput(a,store,booleon){
+    if(booleon){
+        let elem = document.querySelector(`#items_${a}`)
+        elem.innerHTML=`<input style="width: 15%" value="${store[a].Name}" class="input1"></input><input class="input2" value="${store[a].Craeted}" type="date"></input><select  class="input3">
+        <option>Task </option>
+        <option>Random Thought</option>
+        <option>Target</option>
+        <option selected="selected" value="${store[a].Categori}">${store[a].Categori}</option>
+        <option>Idea</option>
+      </select><textarea class="input4">${store[a].Content}</textarea><textarea class="input5"></textarea><button class='add'>change</button><div></div>`  
+    
+    }else if(booleon==false){
+        let date=takeDataInput()
+        // let elem = document.querySelector(`#items_${a}`)
+        // elem.remove()
+        return date
+    }
+    }
+export { changeInput}
+
+
+function removeInput (){
+    const newElement = document.getElementById("input")
+    newElement.remove()
+}
+export { removeInput }
+
+
+
+function remove(store, condition) {
+    if (condition) {
+        store.map(function (items, id) {
+            if (items.Statuse == true) {
+                let elem = document.querySelector(`#items_${id}`)
+                elem.remove()
+            }
+        })
+    } else if (condition == false) {
+        store.map(function (items, id) {
+            if (items.Statuse == false) {
+                let elem = document.querySelector(`#items_${id}`)
+                elem.remove()
+            }
+        })
+    }
+    for(let i=0;i<4;i++){
+        const TabeleStatistics = document.querySelector(".rowStatistics")
+        TabeleStatistics.remove()  
+    }
+    const elem = document.querySelector(".addTabele")
+    elem.classList.remove("disappear")
+}
+export { remove }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ store.js
@@ -0,0 +1,133 @@
+import { render, remove, renderArchive,addInput,removeInput,takeDataInput,changeInput } from "./render.js";
+
+
+//Initial Information
+let store = [
+     { Name: "Shopping list", Craeted: "2022-08-24", Categori: "Task", Content: "Milk", Dates: "", Statuse:  true },
+     { Name: 'City', Craeted: "2022-04-23", Categori: "Random Thought", Content: "Go to the museum", Dates: "", Statuse: false },
+     { Name: "Book", Craeted: "2022-09-17", Categori: "Random Thought", Content: "Read something", Dates: "", Statuse: true },
+     { Name: "life", Craeted: "2022-03-28", Categori: "Target", Content: "Run 1 km", Dates:" 3/5/2021, 5/5/2021", Statuse: true },
+     { Name: "Create App", Craeted: "2022-01-26", Categori: "Idea", Content:"Create to do list", Dates: "", Statuse:false },
+     { Name: "Shopping list", Craeted: "2022-05-18", Categori: "Task", Content: "Porridge", Dates: "", Statuse: true },
+     { Name: "life", Craeted: "2022-03-24", Categori: "Random Thought", Content: "I am the best", Dates: "", Statuse: true },
+]
+
+//initial rendering
+render(store, true)
+eventDelegation()
+
+
+//add button
+let addButton = document.querySelector(".addTabele")
+function showInput(){
+    addInput();
+    addButton.addEventListener("click", addDate)
+    addButton.removeEventListener("click",showInput)
+    eventDelegation()
+}
+function addDate(){
+    remove(store, true)
+    let a=takeDataInput()
+    store.push(a)
+    removeInput()
+    render(store, true)
+    eventDelegation()
+    addButton.addEventListener("click",  showInput)
+    addButton.removeEventListener("click",  addDate)
+}
+addButton.addEventListener("click", showInput)
+
+
+//show and hide archive button
+let archiveButton = document.querySelector(".archive")
+function showArchive() {
+    archiveButton.addEventListener("click", hideArchive)
+    archiveButton.removeEventListener("click", showArchive)
+    remove(store, true)
+    renderArchive(store,false)
+    eventDelegation()
+}
+function hideArchive() {
+    archiveButton.addEventListener("click", showArchive)
+    archiveButton.removeEventListener("click", hideArchive)
+    remove(store, false)
+    render(store, true)
+    eventDelegation()
+}
+archiveButton.addEventListener("click", showArchive)
+
+
+
+
+
+
+
+function eventDelegation() {
+
+    let removeButton = document.querySelectorAll(".remove")
+    function deleteItems() {
+        let a = event.currentTarget.closest(".row").id.substring(6);
+        if (store[a].Statuse) {
+            remove(store, true)
+            store.splice(a, 1)
+            render(store, true)
+            eventDelegation()
+        } else if (store[a].Statuse == false) {
+            remove(store, false)
+            store.splice(a, 1)
+            renderArchive(store, false)
+            eventDelegation()
+        }
+    }
+    removeButton.forEach(it => {
+        it.addEventListener("click", deleteItems)
+    })
+
+
+    let changeButton = document.querySelectorAll(".change")
+    function changeItems() {
+         let a = event.currentTarget.closest(".row").id.substring(6);
+         changeInput(a, store,true)
+        eventDelegation()
+    }
+    changeButton.forEach(it => {
+        it.addEventListener("click", changeItems)
+    })
+
+
+   let addChange = document.querySelectorAll(".add")
+    function changeDate() {
+         let a = event.currentTarget.closest(".row").id.substring(6);
+        let data= changeInput(a, store,false)
+        store[a]=data
+        console.log(data)
+        remove(store, true)
+         render(store, true)
+        eventDelegation()
+    }
+    addChange.forEach(it => {
+        it.addEventListener("click",changeDate)
+    })
+
+
+    let archiveButton = document.querySelectorAll(".onArchive")
+    function onArchive() {
+        let a = event.currentTarget.closest(".row").id.substring(6);
+        if (store[a].Statuse != false) {
+            remove(store,true)
+            store[a].Statuse = false
+            render(store,true)
+            eventDelegation()
+        } else if (store[a].Statuse == false) {
+            remove(store, false)
+            store[a].Statuse = true
+            renderArchive(store, false)
+            eventDelegation()
+        }
+    }
+    archiveButton.forEach(it => {
+        it.addEventListener("click", onArchive)
+    })
+}
+
+
